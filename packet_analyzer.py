2893b6c6cab85b25e05c4dc702abae0bd25fa81e
drb6472 <113384589+drb6472@users.noreply.github.com>
2023-04-24 15:51:32 -0400

    Update packet_analyzer.py
    Added auxilary code

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,3 +1,4 @@
+#filter,parse, and metric functions
 def filter(fn):
     print('called filter function in filter_packets.py')
     read(fn)
@@ -36,7 +37,32 @@ def main():
     parseList = (parse('debug.txt'))
     for packet in parseList:
         print(packet)
+ #This is where we call the functions and ask for parameters
+from filter_packets import *
+from packet_parser import *
+from compute_metrics import *
 
+import sys
+
+fileName = sys.argv[1]
+
+L = []
+ip = "192.168.100.1"
+
+if( "1" in fileName):
+    ip = "192.168.100.1"
+elif( "2" in fileName):
+    ip = "192.168.100.2"
+elif( "3" in fileName):
+    ip = "192.168.200.1"
+elif( "4" in fileName):
+    ip = "192.168.200.2"
+elif( "5" in fileName):
+    ip = "192.168.100.254"
+
+filter(fileName)
+parse(fileName)
+compute(L, ip, fileName)
 if __name__ == '__main__':
     main()
 
