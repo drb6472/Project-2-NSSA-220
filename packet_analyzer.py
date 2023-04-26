1e693ab414ec0ffff19f5521d45cb3fc3eb1ddfe
drb6472 <113384589+drb6472@users.noreply.github.com>
2023-04-26 15:57:51 -0400

    Update packet_analyzer.py

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,26 +1,26 @@
 #filter,parse, and metric functions
 def filter(fn):
-    print('called filter function in filter_packets.py')
-    read(fn)
+	print 'called filter function in filter_packets.py'
+	read(fn)
 
-def read(filename):
-    mid_step = filename.split(".")
-    nfile = mid_step[0] + "_filtered." + mid_step[1]
-    
-    unfnode = open(filename, "r")
-    fnode = open(nfile, "w")
-    fnode.close()
-    fnode = open(nfile, "a")
-    line = unfnode.readline()
-    
-    while line:
-        if "unreachable" not in line:
-            if "ICMP" in line:
-                fnode.write(line)
-        line = unfnode.readline()
+def read(file_name):
+	mid_step = file_name.split(".")
+	new_file_name = mid_step[0] + "_filtered." + mid_step[1]
+	
+	unfiltered_node = open(file_name, "r")
+	filtered_node = open(new_file_name, "w")
+	filtered_node.close()
+	filtered_node = open(new_file_name, "a")
+	line = unfiltered_node.readline()
+	
+	while line:
+		if "unreachable" not in line:
+			if "ICMP" in line:
+				filtered_node.write(line)
+		line = unfiltered_node.readline()
 
-    unfnode.close()
-    fnode.close()
+	unfiltered_node.close()
+	filtered_node.close()
 
 def parse(filename):
     parseList = []
