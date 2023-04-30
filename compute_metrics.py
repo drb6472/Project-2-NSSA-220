4640ea585f2b78bebb5ff27483bdfe429ef68eef
Justin Huang <59941045+jznn@users.noreply.github.com>
2023-04-30 04:41:25 -0400

    Create compute_metrics.py
    finished data size metrics, still need to do time based

---
+++ compute_metrics.py
@@ -0,0 +1,42 @@
+def compute(parsedList, ip, filename):
+    print('called compute function in compute_metrics.py')
+    midstep = filename.split(".")
+    newfilename= midstep[0] + "_filtered.txt"
+    file = open(newfilename, "r")
+    for line in file:
+        node.append(line.split())
+    file.close()
+    reqsent = 0
+    reqrcvd = 0
+    repsent = 0
+    reprcvd = 0
+    reqdatasent = 0
+    reqdatarcvd = 0
+    repbytessent = 0
+    repbytesrcvd = 0
+    for i in parsedList:
+        if i[8] == "reply":
+            if i[2] == ip:
+                repsent = repsent + 1
+            elif i[3] == ip:
+                reprcvd = reprcvd + 1
+        if i[8] == "request":
+            if i[2] == ip:
+                reqsent = reqsent + 1
+                reqbytessent = reqbytessent + int(i[5])
+                reqdatasent = reqdatasent + int(i[5]) - 42
+            elif i[3] == ip:
+                reqrcvd = reqrcvd + 1
+                reqbytesrcvd = reqbytesrcvd + int(i[5])
+                reqdatarcvd = reqdatarcvd + int(i[5]) - 42
+ 
+    output = open("output.csv", "a")
+    output.write(outputName)
+    output.write("  \n")
+    output.write("Echo Requests Sent" + "," + "Echo Requests Recieved" + "," + "Echo Replies Sent" + "," + "Echo Replies Recieved\n" )
+    output.write(str(reqsent/2) + "," + str(reqrcvd/2) + "," + str(repsent/2) + "," + str(reprcvd/2) + "\n")
+    output.write("Echo Request Bytes sent (bytes)" + "," + "Echo Request Data Sent (bytes)\n")
+    output.write(str(reqbytessent/2) + "," + str(reqdatasent/2) + "\n")
+    output.write("Echo Request Bytes Recieved (bytes)" + "," + "Echo Request Data Recieved (bytes)\n")
+    output.write(str(reqbytesrcvd/2) + "," + str(reqdatarcvd/2) + "\n")
+    output.write(" \n")
