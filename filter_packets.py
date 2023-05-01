f0f0c73a40e6cc2dd9e7ec871721a29850347757
Justin Huang <59941045+jznn@users.noreply.github.com>
2023-04-30 22:34:23 -0400

    Update filter_packets.py
    minor change - added return statement

--- filter_packets.py
+++ filter_packets.py
@@ -1,23 +1,23 @@
 def filter(fn):
     print('called filter function in filter_packets.py')
-    read(fn)
+    return read(fn)
 
-def read(filename):
-    mid_step = filename.split(".")
-    nfile = mid_step[0] + "_filtered." + mid_step[1]
+def read(file_name):
+    mid_step = file_name.split(".")
+    new_file_name = mid_step[0] + "_filtered." + mid_step[1]
     
-    unfnode = open(filename, "r")
-    fnode = open(nfile, "w")
-    fnode.close()
-    fnode = open(nfile, "a")
-    line = unfnode.readline()
+    unfiltered_node = open(file_name, "r")
+    filtered_node = open(new_file_name, "w")
+    filtered_node.close()
+    filtered_node = open(new_file_name, "a")
+    line = unfiltered_node.readline()
     
     while line:
         if "unreachable" not in line:
             if "ICMP" in line:
-                fnode.write(line)
-        line = unfnode.readline()
+                filtered_node.write(line)
+        line = unfiltered_node.readline()
 
-    unfnode.close()
-    fnode.close()
-
+    unfiltered_node.close()
+    filtered_node.close()
+    return new_file_name
