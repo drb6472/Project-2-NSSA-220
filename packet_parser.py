6fafb9f3daedda9bb61aae6f1671f64701661d82
Justin Huang <59941045+jznn@users.noreply.github.com>
2023-04-30 22:35:07 -0400

    Update packet_parser.py

--- packet_parser.py
+++ packet_parser.py
@@ -1,18 +1,10 @@
 def parse(filename):
+    print('called parse function in packet_parser.py')
     parseList = []
     with open(filename) as f:
         content = f.readlines()
         for line in content:
             line = line.rstrip()
-            line = line.split(",")
+            line = line.split()
             parseList.append(line)
     return parseList
-
-
-def main():
-    parseList = (parse('debug.txt'))
-    for packet in parseList:
-        print(packet)
-
-if __name__ == '__main__':
-    main()
