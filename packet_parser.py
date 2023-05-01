8b04ea6a54594d92b8795ae2fd1699aa7f13126a
jonwong966 <32588285+jonwong966@users.noreply.github.com>
2023-05-01 19:21:37 -0400

    optimized and tried hex parsing. did not work :(

--- packet_parser.py
+++ packet_parser.py
@@ -2,9 +2,20 @@ def parse(filename):
     print('called parse function in packet_parser.py')
     parseList = []
     with open(filename) as f:
+        for line in f:
+            parseList.append(line.rstrip().split())
+    return parseList
+
+# This is for hex but did not complete 
+# def parse(filename):
+    print('called parse function in packet_parser.py')
+    parseList = []
+    with open(filename, "rb") as f:
         content = f.readlines()
         for line in content:
-            line = line.rstrip()
-            line = line.split()
-            parseList.append(line)
+            hex_values = []
+            for byte in line:
+                hex_value = hex(byte)[2:].zfill(2)  # Convert byte to hex string
+                hex_values.append(hex_value)
+            parseList.append(hex_values)
     return parseList
