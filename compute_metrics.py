ffa3f7a8888dadfcd0159cdb59339761d3682f7d
Justin Huang <59941045+jznn@users.noreply.github.com>
2023-04-30 22:33:56 -0400

    Update compute_metrics.py
    added portions for time and distanced based metrics (distance needs to be fixed)

--- compute_metrics.py
+++ compute_metrics.py
@@ -6,14 +6,23 @@ def compute(parsedList, ip, filename):
     for line in file:
         parsedList.append(line.split())
     file.close()
+    # defining variables to be used
     reqsent = 0
     reqrcvd = 0
     repsent = 0
     reprcvd = 0
     reqdatasent = 0
     reqdatarcvd = 0
-    repbytessent = 0
-    repbytesrcvd = 0
+    reqbytessent = 0
+    reqbytesrcvd = 0
+    total = 0
+    rttCount = 0
+    avgdelayreply = 0
+    delaycount = 0
+    hop = 0
+    requestcount = 0
+
+    # data size metrics
     for i in parsedList:
         if i[8] == "reply":
             if i[2] == ip:
@@ -29,7 +38,31 @@ def compute(parsedList, ip, filename):
                 reqrcvd = reqrcvd + 1
                 reqbytesrcvd = reqbytesrcvd + int(i[5])
                 reqdatarcvd = reqdatarcvd + int(i[5]) - 42
- 
+
+    # time based metrics 
+    for i in range(0, len(parsedList), 2):
+        if parsedList[i][2] == ip and parsedList[i][8] == "request":
+            total += (float(parsedList[i+1][1]) - float(parsedList[i][1]))
+            rttCount += 1
+        if parsedList[i][3] == ip and parsedList[i][8] == "request":
+            avgdelayreply += float(parsedList[i+1][1]) - float(parsedList[i][1])
+            delaycount += 1
+    rttTotal = 1000 * (total / rttCount)
+    echoreqthroughput = reqbytessent/total/1000
+    echoreqgoodput = reqdatasent/total/1000
+    avgdelayreply = ((avgdelayreply/delaycount) * 1000000)
+
+    # distance based metric 
+    for i in parsedList:
+        if i[8] == "request":
+            if "126" in i[11]:
+                hop += 1
+            else:
+                hop += 3
+        requestcount += 1
+    hop = float(hop) / float(requestcount)
+
+    # writing output to file
     outputName = "output.csv"
     with open(outputName, "a") as output:
         output.write(newfilename)
@@ -40,4 +73,10 @@ def compute(parsedList, ip, filename):
         output.write(str(reqbytessent/2) + "," + str(reqdatasent/2) + "\n")
         output.write("Echo Request Bytes Received (bytes)" + "," + "Echo Request Data Received (bytes)\n")
         output.write(str(reqbytesrcvd/2) + "," + str(reqdatarcvd/2) + "\n")
+        output.write(" \n")
+        output.write("Average RTT (miliseconds)" + "," + str(rttTotal) + "\n")
+        output.write("Echo Request Throughput (kB/sec)" + "," + str(echoreqthroughput) + "\n")
+        output.write("Echo Request Goodput (kB/sec)" + "," + str(echoreqgoodput) + "\n")
+        output.write("Average Reply Delay" + "," + str(avgdelayreply) + "\n")
+        output.write("Average Echo Request Hop Count" + "," + str(hop) + "\n")
         output.write(" \n")
