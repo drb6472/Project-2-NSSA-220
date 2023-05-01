91add6131238e941cb0536f9c54236a2846e4633
Justin Huang <59941045+jznn@users.noreply.github.com>
2023-04-30 22:34:44 -0400

    Update packet_analyzer.py
    removed testing code

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -1,43 +1,8 @@
 #filter,parse, and metric functions
-def filter(fn):
-    print('called filter function in filter_packets.py')
-    return read(fn)
-
-def read(file_name):
-    mid_step = file_name.split(".")
-    new_file_name = mid_step[0] + "_filtered." + mid_step[1]
-    
-    unfiltered_node = open(file_name, "r")
-    filtered_node = open(new_file_name, "w")
-    filtered_node.close()
-    filtered_node = open(new_file_name, "a")
-    line = unfiltered_node.readline()
-    
-    while line:
-        if "unreachable" not in line:
-            if "ICMP" in line:
-                filtered_node.write(line)
-        line = unfiltered_node.readline()
-
-    unfiltered_node.close()
-    filtered_node.close()
-    return new_file_name
-
-def parse(filename):
-    print('called parse function in packet_parser.py')
-    parseList = []
-    with open(filename) as f:
-        content = f.readlines()
-        for line in content:
-            line = line.rstrip()
-            line = line.split(",")
-            parseList.append(line)
-    return parseList
-
-
-
- #This is where we call the functions and ask for parameters
-
+#This is where we call the functions and ask for parameters
+from filter_packets import *
+from packet_parser import *
+from compute_metrics import *
 import sys
 
 fileName = sys.argv[1]
@@ -57,5 +22,4 @@ elif "5" in fileName:
 
 filtered = filter(fileName)
 parseList = parse(filtered)
-# compute(parseList, ip, fileName) # compute function is not defined
-
+compute(parseList, ip, fileName) 
